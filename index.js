7fdfff5858dbb8eba5ec4ede1f947616aee10a30
Kiiroi <sammydevjs@gmail.com>
2022-11-28 06:49:08 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,92 @@
+// VARIÁVEIS
+const pokemonDoVisor = document.querySelector('#pokemonGif'); // Este é o pokemon que aparece no visor
+const campoPesquisa = document.querySelector('#pesquisaPokemon');
+const botaoPesquisa = document.querySelector('#pesquisaPokemonBtn');
+let pokemonNome = 'bulbasaur';
+
+// ================================================== ARMAZENANDO A API EM UMA VARIÁVEL ==================================================
+async function poke(nome){
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`).then(response => response.json());
+}
+// ================================================== CAPTURANDO E APAGANDO VALOR DO INPUT ==================================================
+botaoPesquisa.addEventListener('click', () => {
+  pokemonNome = campoPesquisa.value; // Armazena o valor do input na variável pokemonNome
+  campoPesquisa.value = ''; // Apaga o valor do input
+  pokemonDoVisor.src = `./img/anim/${pokemonNome}.gif` // Altera a imagem do pokemon de acordo com o nome
+  poke(pokemonNome);
+})
+
+
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+// campoPesquisa.addEventListener('keydown', (e)=>{
+//   e = campoPesquisa.value; // Aqui é onde eu pego a primeira letra do nome do pokemon e coloco em maiúsculo
+//   campoPesquisa.value.charAt(0) = e; // Aqui é onde eu coloco a primeira letra do nome do pokemon em maiúsculo
+// })
+// campoPesquisa.value.extConcharAt(0).toUpperCase(); // Aqui é onde eu pego a primeira letra do nome do pokemon e coloco em maiúsculo
+
+
+
+
+
+
+
+// const card = document.querySelector('.cardPokemon');
+// const imagem = document.querySelector('.imagem');
+// const nome = document.querySelector('.nome');
+// const habUm = document.querySelector('.habilidadeUm');
+// const habDois = document.querySelector('.habilidadeDois');
+// const habTres = document.querySelector('.habilidadeTres');
+// const campo = document.querySelector('#escrever');
+// const botao = document.querySelector('#botao')
+
+
+// // campo.value = nome;
+
+// async function buscandoAPI(parametro){
+//   const teste = await fetch(`https://pokeapi.co/api/v2/pokemon/${parametro}`).then(res => res.json())
+//   // console.log(teste.abilities[1].ability.name)
+  
+//   if (teste.abilities[1] == undefined){
+//     let hability2 = ""
+//     habDois.textContent = hability2;
+  
+//   } else {
+//     let hability2 = teste.abilities[1].ability.name
+//     habDois.textContent = hability2;
+//   }
+
+//   if (teste.abilities[2] == undefined){
+//     let hability3 = ""
+//     habTres.textContent = hability3;
+  
+//   } else {
+//     let hability3 = teste.abilities[2].ability.name
+//     habTres.textContent = hability3;
+//   }
+
+//   imagem.src = teste.sprites.other.home.front_default
+//   nome.textContent = teste.name
+//   habUm.textContent = teste.abilities[0].ability.name
+
+//   }
+  
+//   botao.addEventListener('click', ()=>{
+//     let procurando = campo.value;
+//     campo.value = '';
+//     buscandoAPI(procurando)
+//   })
